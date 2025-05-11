97aa29471bb414b3b35dc06a386b091ff498e7da
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-05-11 03:51:44 +0200

    Refactor HTML and manifest for consistency and clarity; add Tailwind CSS configuration

---
+++ note-frontend/tailwind.config.ts
@@ -0,0 +1,16 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography'
+
+const config: Config = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography],
+}
+
+export default config
